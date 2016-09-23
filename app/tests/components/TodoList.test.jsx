503d0acca58e50e56777e744eaf231509eadcdfe
Rob Pataki <rob@handcrafteddigital.london>
2016-09-23 10:08:22 +0100

    Add basic unit tests for components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+var expect = require('expect');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render 1 todo component for each Todo item', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Todo #1'
+      }, {
+        id: 2,
+        text: 'Todo #2'
+      }, {
+        id: 3,
+        text: 'Todo #3'
+      }
+    ];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todoComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todoComponents.length).toBe(todos.length);
+  });
+});
